e057e5226d142ac9a31c24328ca80f474e292f2b
Scott  Keplinger <skeplin17@gmail.com>
2020-06-01 20:57:40 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3,"male", "brown", "friendly", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 4, "male", "black", "stoic", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 5, "female", "brown", "silly", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 3, "male", "brown", "friendly", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 2, "female", "black", "hungry", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 1, "male", "brown", "grumpy", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 3, "female", "brown", "friendly", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 3, "male", "brown", "aggressive", "true");
